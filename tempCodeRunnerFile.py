e019e1fb09032abf1ec2d74d4e45bdcdf274636d
DICEMANE <119860738+DICEMANE@users.noreply.github.com>
2023-01-31 09:22:08 +0200

    woman make

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,5 @@
-e = self.woman_top1, tags='drebe')
-            # top2 = c.create_image(560, 153, image = self.woman_top2, tags='drebe')
-            # top3 = c.create_image(692, 163, image = self.woman_top3, tags='drebe')
+815, 394
+            elif pic == 17:
+                x=185
+                y=503
+                x_start, y_start =813, 475
